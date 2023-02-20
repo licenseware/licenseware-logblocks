9b545cd30db36aa3a0da0a409fef0d2a34931f36
ClimenteA <climente.alin@gmail.com>
2023-02-20 15:21:17 +0200

    feat: create markdown msg and push to slack

--- ReadMe.md
+++ ReadMe.md
@@ -15,8 +15,13 @@ When `tail` is called on generated log file push strings into `licenseware_logbl
 tail -f /tmp/awesome-app.log | python3 licenseware_log_blocks.py
 ```
 
+SLACK_TAGGED_USERS_IDS - will be tagged on all ERRORS
+
+
 # Requirements
 
 Environment variables:
 - `SLACK_TAGGED_USERS_IDS` (ex: `export SLACK_TAGGED_USERS_IDS="<@U02CS9QL0JK>, <@U02U2KQ7N3Y>, <@U030JAJF5RV>, <@U02SDCAHJH3>, <@UHW04RBGT>"`);
 - `SLACK_CHANNEL_WEBHOOK_URL` (ex: `export SLACK_CHANNEL_WEBHOOK_URL=https://hooks.slack.com/services/etc/etc/etc`)    
+
+
--- licenseware_log_blocks.py
+++ licenseware_log_blocks.py
@@ -2,8 +2,8 @@ import os
 import re
 import json
 import logging
+from enum import Enum
 from urllib import request, parse
-import traceback
 
 log = logging.getLogger(__name__)
 log.setLevel(level=logging.INFO)
@@ -12,153 +12,51 @@ SLACK_TAGGED_USERS_IDS = os.environ["SLACK_TAGGED_USERS_IDS"]
 SLACK_CHANNEL_WEBHOOK_URL = os.environ["SLACK_CHANNEL_WEBHOOK_URL"]
 
 
-emoji_map = {
-    "DEBUG": ":bug:",
-    "INFO": ":information_source:",
-    "SUCCESS": ":white_check_mark:",
-    "WARNING": ":warning:",
-    "ERROR": ":x:",
-}
+class Emoji(str, Enum):
+    DEBUG = ":bug:"
+    INFO = ":information_source:"
+    SUCCESS = ":white_check_mark:"
+    WARNING = ":warning:"
+    ERROR = ":x:"
 
 
-def compose_message_block(event: str) -> json:
-    """
-    Compose a message block for Slack.
-    """
+def get_formated_slack_message(emoji: Emoji, log_level: str, rawinput: str):
 
-    print(f"compose_message_block: {event}")
-
-    message_block = {"type": "section", "text": {"type": "mrkdwn", "text": ""}}
-    empty_message = re.findall(r"^\d{2}:\d{2}:\d{2}\s+web\.\d{1}\s+\|\s*$", event)
-    if empty_message:
-        return
-
-    message_block = parse_message(event["logGroup"], log_event["message"])
-
-    for log_event in event["logEvents"]:
-        empty_message = re.findall(
-            r"^\d{2}:\d{2}:\d{2}\s+web\.\d{1}\s+\|\s*$", log_event["message"]
-        )
-        if empty_message:
-            continue
-        # TODO - how is the event a dict? What are the origins of 'logGroup', 'message' keys?
-        message_block = parse_message(event["logGroup"], log_event["message"])
-        yield json.dumps(message_block, default=str)
-
-
-def parse_message(log_group, log_message):
-    app_id = (
-        log_group[log_group.rfind("-", 0, log_group.rfind("-")) + 1 :].lstrip().rstrip()
-    )
-    try:
-        log_level = re.search(r"\[([A-Z]+)\]", log_message).group(1)
-        emoji = emoji_map.get(log_level, ":question:")
-    except Exception:
-        log.warning(traceback.format_exc())
-        log_level = None
-
-    try:
-        log_metadata = re.search(r"\[\++ METADATA: ({.*}) \++\]", log_message).group(1)
-        log_metadata = log_metadata.replace("'", '"').replace("None", "null").strip()
-        meta_dict = json.loads(log_metadata)
-    except Exception:
-        log.warning(traceback.format_exc())
-        meta_dict = None
-
-    try:
-        message = re.search(r"\[\*+ MESSAGE: (.*)", log_message).group(1)
-    except Exception:
-        log.warning(traceback.format_exc())
-        message = log_message
-
-    try:
-        code_location = re.search(r"\[\-+ (\S+) -+\]", log_message).group(1)
-    except Exception:
-        log.warning(traceback.format_exc())
-        code_location = "-"
-
-    if meta_dict:
-        message_blocks = return_message_block(
-            emoji, app_id, log_level, message, code_location, meta_dict
-        )
-    else:
-        message_blocks = return_normal_message(message)
-
-    message_blocks = tag_message(message, message_blocks, log_group)
-    return message_blocks
-
-
-def parse_metadata(log_metadata):
-    """Convert metadata dictionary to beautified markdown string"""
-    md_string = ""
-    for key, value in log_metadata.items():
-        md_string += f"*{key}:* {str(value)}\n"
-    return md_string
-
-
-def return_message_block(emoji, app_id, log_level, message, code_location, meta_dict):
-    message_block = {
+    msg = {
         "blocks": [
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": f"{emoji} {log_level} on {app_id.upper()}",
-                    "emoji": True,
-                },
-            },
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f" :mag_right: *Code Location:* {code_location}",
-                    }
-                ],
-            },
-            {
-                "type": "context",
-                "elements": [
-                    {
-                        "type": "mrkdwn",
-                        "text": parse_metadata(meta_dict),
-                        "unfurl_links": False,
-                        "unfurl_media": False,
-                    }
-                ],
-            },
-            {"type": "section", "text": {"type": "mrkdwn", "text": f"```{message}```"}},
             {"type": "divider"},
         ]
     }
-    return message_block
 
+    special_mentions = emoji == Emoji.ERROR
 
-def return_normal_message(message):
-    return {
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"```{message}```",
-                },
-            },
-        ]
+    msg_title = (
+        f"{emoji} {log_level} {'please investigate!' if special_mentions else ''}"
+    )
+
+    msg_header = {
+        "type": "header",
+        "text": {
+            "type": "plain_text",
+            "text": msg_title,
+            "emoji": True,
+        },
     }
 
+    msg_content = {
+        "type": "context",
+        "elements": [
+            {
+                "type": "mrkdwn",
+                "text": f"```{rawinput}```",
+            }
+        ],
+    }
 
-def return_special_mentions():
-    blocks = [
-        {
-            "type": "header",
-            "text": {
-                "type": "plain_text",
-                "text": ":tada: A user is processing data on production.",
-                "emoji": True,
-            },
-        },
-        {
+    msg["blocks"].extend([msg_header, msg_content])
+
+    if special_mentions:
+        msg_mentions = {
             "type": "context",
             "elements": [
                 {
@@ -166,64 +64,41 @@ def return_special_mentions():
                     "text": f":military_helmet: *Special Mentions:* {SLACK_TAGGED_USERS_IDS}",
                 }
             ],
-        },
-    ]
-    return blocks
+        }
+        msg["blocks"].extend([msg_mentions])
+
+    return msg
 
 
-def return_error_alert():
-    blocks = [
-        {
-            "type": "header",
-            "text": {
-                "type": "plain_text",
-                "text": ":x: An error has occurred!",
-                "emoji": True,
-            },
-        },
-        {
-            "type": "context",
-            "elements": [
-                {
-                    "type": "mrkdwn",
-                    "text": f" :military_helmet: *Special Mentions:* {SLACK_TAGGED_USERS_IDS}",
-                }
-            ],
-        },
-    ]
-    return blocks
+def found_error(rawinput: str):
+    return bool(
+        re.search("Traceback (most recent call last):", rawinput)
+        or re.search("ERROR", rawinput)
+    )
 
 
-def tag_message(message, message_blocks, log_group):
-    error_regexes = [
-        r".*Traceback.*",
-        r".*Exception:.*",
-        r".*Error:.*",
-        r".*Warning:.*",
-        r".*failsafe_decorator\.py.*",
-    ]
+def found_debug(rawinput: str):
+    return bool(re.search("DEBUG", rawinput))
 
-    uploads_regexes = [
-        r".*APP PROCESSING EVENT.*",
-    ]
 
-    if (
-        any(re.search(regex, message) for regex in error_regexes)
-        and "prod" in log_group
-    ):
+def found_warning(rawinput: str):
+    return bool(re.search("WARNING", rawinput))
 
-        error_blocks = return_error_alert()
-        message_blocks["blocks"].extend(error_blocks)
 
-    if (
-        any(re.search(regex, message) for regex in uploads_regexes)
-        and "prod" in log_group
-    ):
+def get_emoji_and_log_level(rawinput: str):
+    if found_error(rawinput):
+        return Emoji.ERROR, "ERROR"
+    if found_debug(rawinput):
+        return Emoji.DEBUG, "DEBUG"
+    if found_warning(rawinput):
+        return Emoji.WARNING, "WARNING"
+    return Emoji.INFO, "INFO"
 
-        uploads_blocks = return_special_mentions()
-        message_blocks["blocks"].extend(uploads_blocks)
 
-    return message_blocks
+def get_slack_message(rawinput: str):
+    emoji, log_level = get_emoji_and_log_level(rawinput)
+    slack_message = get_formated_slack_message(emoji, log_level, rawinput)
+    return slack_message
 
 
 def post_message(message):
@@ -246,7 +121,8 @@ if __name__ == "__main__":
     import sys
 
     try:
-        slack_message = compose_message_block(sys.stdin.readline())
-        # post_message(slack_message)
+        rawinput = sys.argv[0]
+        slack_message = get_slack_message(rawinput)
+        post_message(slack_message)
     except Exception as err:
         log.error(err)
