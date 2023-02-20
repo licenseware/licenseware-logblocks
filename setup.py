fd9a7c66ea4717335f63bef777fce427744cff48
ClimenteA <climente.alin@gmail.com>
2023-02-20 15:24:40 +0200

    fix: updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from setuptools import setup, find_packages
 
 # python3 setup.py bdist_wheel sdist
 
-with open("requirements.txt", "r") as f:
-    REQUIREMENTS = f.readlines()
-
 setup(
     name="licenseware-logblocks",
     version="0.0.1",
@@ -13,5 +10,4 @@ setup(
     author_email="contact@licenseware.com",
     description="Post formated log message to slack",
     packages=find_packages(),
-    install_requires=REQUIREMENTS,
 )
